796e495163e901e25363f5fc6e4815c9fe97b3b1
AsazadPathan31 <pathanasazad29@gmail.com>
2025-06-17 00:01:18 +0530

    UI Style changes

--- blogging/src/app/app.module.ts
+++ blogging/src/app/app.module.ts
@@ -1,19 +1,23 @@
-import { NgModule,NO_ERRORS_SCHEMA,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+	NgModule,
+	NO_ERRORS_SCHEMA,
+	CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireModule } from "@angular/fire/compat";
-import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from 'src/environments/environment';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { MatListModule} from '@angular/material/list'
-import { MatIconModule} from '@angular/material/icon';
-import { MatFormFieldModule} from '@angular/material/form-field';
-import { MatToolbarModule} from '@angular/material/toolbar';
-import { MatCardModule} from '@angular/material/card'
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { CreateEditComponent } from './create-edit/create-edit.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -25,44 +29,40 @@ import { ReadComponent } from './read/read.component';
 import { FormsModule } from '@angular/forms';
 import { AdminComponent } from './admin/admin.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SidebarComponent,
-    CreateEditComponent,
-    HomepageComponent,
-    ProfileComponent,
-    ReadComponent,
-    AdminComponent,
-  ],
-  imports: [
-    NoopAnimationsModule,
-    BrowserAnimationsModule,
-    RouterModule,
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    CKEditorModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatToolbarModule,
-    MatCardModule,
-    MatInputModule,
-    MatListModule,
-    MatIconModule,
-
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA,
-    NO_ERRORS_SCHEMA
-  ]
-
+	declarations: [
+		AppComponent,
+		SidebarComponent,
+		CreateEditComponent,
+		HomepageComponent,
+		ProfileComponent,
+		ReadComponent,
+		AdminComponent,
+	],
+	imports: [
+		BrowserAnimationsModule,
+		RouterModule,
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		AngularFireModule.initializeApp(environment.firebaseConfig),
+		AngularFirestoreModule,
+		AngularFireAuthModule,
+		CKEditorModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		MatToolbarModule,
+		MatCardModule,
+		MatInputModule,
+		MatListModule,
+		MatIconModule,
+		MatButtonModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
+	schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
