10d2722cb8563f1856f7bdc7d92d14b8d6b9ffae
AsazadPathan31 <pathanasazad29@gmail.com>
2025-06-16 22:21:53 +0530

    Import Resolved for the routing

--- blogging/src/app/app-routing.module.ts
+++ blogging/src/app/app-routing.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CreateEditComponent } from './create-edit/create-edit.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ReadComponent } from './read/read.component';
 // Task 1: Import components here
 
 const routes: Routes = [
-  {path:"blogs", component: HomepageComponent},
-  {path:"", component: HomepageComponent},
-  {path:"create", component: CreateEditComponent},
-  {path:"adminlogin",component:AdminComponent},
-  {path:"profile", component: ProfileComponent},
-  {path:"read/:id", component: ReadComponent, },
-  {path:"edit/:id", component: CreateEditComponent},
+	{ path: 'blogs', component: HomepageComponent },
+	{ path: '', component: HomepageComponent },
+	{ path: 'create', component: CreateEditComponent },
+	{ path: 'adminlogin', component: AdminComponent },
+	{ path: 'profile', component: ProfileComponent },
+	{ path: 'read/:id', component: ReadComponent },
+	{ path: 'edit/:id', component: CreateEditComponent },
 ];
 
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
